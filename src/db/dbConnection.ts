447d365c29185b74999646ecd42f2588f6e17f68
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-09-03 00:31:05 +0900

    DB との Connection 処理を変更

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -5,12 +5,11 @@ import {
   getConnection
 } from 'typeorm';
 
-import util from '@/utils';
-
-import uuid = require('uuid/v1');
+import ormconfig = require('../../ormconfig');
+import uuid = require('uuid');
 
 const disconnect = async (): Promise<void> => {
-  const connection: Connection = getConnection(process.env.DB_CONNECTION_UUID);
+  const connection: Connection = getConnection(process.env.DB_CONNECTION_NAME);
 
   if (!connection.isConnected) {
     await connection.close();
@@ -18,20 +17,13 @@ const disconnect = async (): Promise<void> => {
 };
 
 const connect = async (): Promise<void> => {
-  const connection: Connection = await createConnection(
-    process.env.NODE_ENV === 'development'
-      ? {
-          name: process.env.DB_CONNECTION_UUID = uuid(),
-          ...util('db.development')
-        }
-      : {
-          name: process.env.DB_CONNECTION_UUID = 'default',
-          ...util('db.production')
-        }
-  ).catch(err => {
+  const connection: Connection = await createConnection({
+    name: process.env.DB_CONNECTION_NAME = uuid(),
+    ...ormconfig
+  }).catch(err => {
+    console.log(err);
     throw new Error(err);
   });
-
   BaseEntity.useConnection(connection);
 };
 
