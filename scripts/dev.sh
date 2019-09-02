caa4cec8f88f8a1606b612ac468cc448aa283dbd
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-09-03 00:32:23 +0900

    docker 立ち上げでのエントリーポイントとなる shell を追加

---
+++ scripts/dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export TZ="Asia/Tokyo"
+export NODE_ENV=development
+
+# wait for sonarqube server.
+sonarqubeLink="http://sonarqube:9000"
+until curl $sonarqubeLink > /dev/null 2>&1;
+do
+  >&2 echo "Scanning is not possible until sonarqube starts. - sleeping"
+  sleep 1
+done
+sleep 20
+
+sed -e 's/localhost/sonarqube/6' sonar-project.properties
+npm run scan
+
+# start dev server
+npm run dev
