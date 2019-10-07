128101ee53e0f721e9985e6cf14777a453e1de31
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-10-07 20:33:14 +0900

    詳細のカラムを削除（シーケンスの中でAPIを叩くようにするため）

--- src/entities/Pokemons.ts
+++ src/entities/Pokemons.ts
@@ -1,10 +1,10 @@
 import {
-  Entity,
-  PrimaryColumn,
-  Column,
-  BaseEntity,
-  OneToMany,
-  OneToOne
+    Entity,
+    PrimaryColumn,
+    Column,
+    BaseEntity,
+    OneToMany,
+    OneToOne
 } from 'typeorm';
 import Gifs from './Gifs';
 import Pngs from './Pngs';
@@ -13,59 +13,53 @@ import Specs from './Specs';
 
 @Entity()
 export class Pokemons extends BaseEntity {
-  @PrimaryColumn()
-  public id: number;
+    @PrimaryColumn()
+    public id: number;
 
-  @Column({
-    type: 'varchar',
-    length: 60,
-    unique: true,
-    nullable: true
-  })
-  public code: string = '';
+    @Column({
+        type: 'varchar',
+        length: 60,
+        unique: true,
+        nullable: true
+    })
+    public code: string = '';
 
-  @Column({
-    type: 'varchar',
-    length: 60,
-    nullable: true
-  })
-  public name: string = '';
+    @Column({
+        type: 'varchar',
+        length: 60,
+        nullable: true
+    })
+    public name: string = '';
 
-  @Column({
-    type: 'smallint',
-    name: 'generation_no',
-    nullable: true
-  })
-  public generationNo: number;
+    @Column({
+        type: 'smallint',
+        name: 'generation_no',
+        nullable: true
+    })
+    public generationNo: number;
 
-  @Column({
-    type: 'varchar',
-    length: 255
-  })
-  public detail: string;
+    @OneToMany(() => Gifs, gifs => gifs.pokemon)
+    public gifs: Gifs[];
 
-  @OneToMany(() => Gifs, gifs => gifs.pokemon)
-  public gifs: Gifs[];
+    @OneToOne(() => Specs, spec => spec.pokemon)
+    public spec: Specs;
 
-  @OneToOne(() => Specs, spec => spec.pokemon)
-  public spec: Specs;
+    @OneToOne(() => Pngs, pngs => pngs.pokemon)
+    public pngs: Pngs;
 
-  @OneToOne(() => Pngs, pngs => pngs.pokemon)
-  public pngs: Pngs;
+    @OneToMany(() => PokemonTypes, pokemonTypes => pokemonTypes.pokemon)
+    public pokemonTypes: PokemonTypes[];
 
-  @OneToMany(() => PokemonTypes, pokemonTypes => pokemonTypes.pokemon)
-  public pokemonTypes: PokemonTypes[];
+    constructor(id: number, code: string, name: string) {
+        super();
+        this.id = id;
+        this.code = code;
+        this.name = name;
+    }
 
-  constructor(id: number, code: string, name: string) {
-    super();
-    this.id = id;
-    this.code = code;
-    this.name = name;
-  }
-
-  public refer(): void {
-    console.table(this);
-  }
+    public refer(): void {
+        console.table(this);
+    }
 }
 
 export default Pokemons;
