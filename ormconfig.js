470867f5ce7301ad65ef4a8a9b399b8340059707
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-11-27 01:02:25 +0900

    eslint fix を実行

--- ormconfig.js
+++ ormconfig.js
@@ -1,32 +1,26 @@
 function parseDBConfig(configString) {
-   try {
-       return JSON.parse(configString);
-   } catch {
-       return {};
-   }
-};
+    try {
+        return JSON.parse(configString);
+    } catch {
+        return {};
+    }
+}
 
 const DB_CONFIG = parseDBConfig(process.env.DB_CONFIG);
 
 module.exports = {
-   type: 'postgres',
-   host: DB_CONFIG.host || 'localhost',
-   port: DB_CONFIG.port || 5432,
-   username: DB_CONFIG.username || 'pokemon',
-   password: DB_CONFIG.password || 'pokemon',
-   database: DB_CONFIG.database || 'pokemon',
-   synchronize: true,
-   logging: false,
-   entities: [
-      'src/domain/entities/**/*.ts',
-      'domain/entities/**/*.ts'
-   ],
-   migrations: [
-      'db/migrations/**/*.ts',
-      '../db/migrations/**/*.ts'
-   ],
-   cli: {
-      entitiesDir: 'src/domain/entities',
-      migrationsDir: 'db/migrations'
-   }
+    type: 'postgres',
+    host: DB_CONFIG.host || 'localhost',
+    port: DB_CONFIG.port || 5432,
+    username: DB_CONFIG.username || 'pokemon',
+    password: DB_CONFIG.password || 'pokemon',
+    database: DB_CONFIG.database || 'pokemon',
+    synchronize: true,
+    logging: false,
+    entities: ['src/domain/entities/**/*.ts', 'domain/entities/**/*.ts'],
+    migrations: ['db/migrations/**/*.ts', '../db/migrations/**/*.ts'],
+    cli: {
+        entitiesDir: 'src/domain/entities',
+        migrationsDir: 'db/migrations'
+    }
 };
