5fce6f5c84e4febd2f7b7b08ba23e550db6816c8
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-11-29 20:51:10 +0900

    環境ごとの接続設定に修正

--- ormconfig.js
+++ ormconfig.js
@@ -8,19 +8,40 @@ function parseDBConfig(configString) {
 
 const DB_CONFIG = parseDBConfig(process.env.DB_CONFIG);
 
-module.exports = {
-    type: 'postgres',
-    host: DB_CONFIG.host || 'localhost',
-    port: DB_CONFIG.port || 5432,
-    username: DB_CONFIG.username || 'pokemon',
-    password: DB_CONFIG.password || 'pokemon',
-    database: DB_CONFIG.database || 'pokemon',
-    synchronize: true,
-    logging: false,
-    entities: ['src/domain/entities/**/*.ts', 'domain/entities/**/*.ts'],
-    migrations: ['db/migrations/**/*.ts', '../db/migrations/**/*.ts'],
-    cli: {
-        entitiesDir: 'src/domain/entities',
-        migrationsDir: 'db/migrations'
+module.exports = [
+    {
+        name: 'development',
+        type: 'postgres',
+        host: DB_CONFIG.host || 'localhost',
+        port: DB_CONFIG.port || 5432,
+        username: DB_CONFIG.username || 'pokemon',
+        password: DB_CONFIG.password || 'pokemon',
+        database: DB_CONFIG.database || 'pokemon',
+        synchronize: true,
+        logging: false,
+        entities: ['src/domain/entities/**/*.ts', 'domain/entities/**/*.ts'],
+        migrations: ['db/migrations/**/*.ts', '../db/migrations/**/*.ts'],
+        cli: {
+            entitiesDir: 'src/domain/entities',
+            migrationsDir: 'db/migrations'
+        }
+    },
+    {
+        // TODO: production 環境が出来上がり次第、ここの設定も変更予定
+        name: 'production',
+        type: 'postgres',
+        host: DB_CONFIG.host || 'localhost',
+        port: DB_CONFIG.port || 5432,
+        username: DB_CONFIG.username || 'pokemon',
+        password: DB_CONFIG.password || 'pokemon',
+        database: DB_CONFIG.database || 'pokemon',
+        synchronize: true,
+        logging: false,
+        entities: ['dist/domain/entities/**/*.js'],
+        migrations: ['db/migrations/**/*.js'],
+        cli: {
+            entitiesDir: 'dist/domain/entities',
+            migrationsDir: 'db/migrations'
+        }
     }
-};
+];
