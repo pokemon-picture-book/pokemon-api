44649da98b1c314827d8aa1f5fba3a499142bc6e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-06-20 12:37:26 +0900

    entity の全件エクスポート処理を追加

---
+++ src/domain/entity/index.ts
@@ -0,0 +1,39 @@
+import Evolution from './Evolution.entity';
+import FlavorTextEntry from './FlavorTextEntry.entity';
+import GameVersion from './GameVersion.entity';
+import GameVersionGroup from './GameVersionGroup.entity';
+import GameVersionName from './GameVersionName.entity';
+import Genera from './Genera.entity';
+import Language from './Language.entity';
+import Pokemon from './Pokemon.entity';
+import PokemonEvolution from './PokemonEvolution.entity';
+import PokemonGameImage from './PokemonGameImage.entity';
+import PokemonImage from './PokemonImage.entity';
+import PokemonName from './PokemonName.entity';
+import PokemonType from './PokemonType.entity';
+import Region from './Region.entity';
+import RegionName from './RegionName.entity';
+import Status from './Status.entity';
+import Type from './Type.entity';
+import TypeName from './TypeName.entity';
+
+export default [
+    Evolution,
+    FlavorTextEntry,
+    GameVersion,
+    GameVersionGroup,
+    GameVersionName,
+    Genera,
+    Language,
+    Pokemon,
+    PokemonEvolution,
+    PokemonGameImage,
+    PokemonImage,
+    PokemonName,
+    PokemonType,
+    Region,
+    RegionName,
+    Status,
+    Type,
+    TypeName
+];
