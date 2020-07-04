3a13b3fb9d1d8437e66fea3198d4607274cb5c74
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-07-04 19:08:14 +0900

    新たに追加されたクラスの DI 設定を追加

--- src/registory/inversify.config.ts
+++ src/registory/inversify.config.ts
@@ -9,12 +9,25 @@ import PokemonController from '@/controller/Pokemon.controller';
 import TYPES from '@/registory/inversify.types';
 import IPokemonPresenter from '@/domain/presenter/IPokemon.presenter';
 import PokemonPresenter from '@/presenter/Pokemon.presenter';
+import ILanguageRepository from '@/domain/repository/ILanguage.repository';
+import LanguageRepository from '@/infrastructure/repository/Language.repository';
+import IGameVersionGroupRepository from '@/domain/repository/IGameVersionGroup.repository';
+import GameVersionGroupRepository from '@/infrastructure/repository/GameVersionGroup.repository';
+import IRegionRepository from '@/domain/repository/IRegion.repository';
+import RegionRepository from '@/infrastructure/repository/Region.repository';
 
 const container = new Container();
 container.bind<IPokemonPresenter>(TYPES.IPokemonPresenter).to(PokemonPresenter);
 container
     .bind<IPokemonRepository>(TYPES.IPokemonRepository)
     .to(PokemonRepository);
+container
+    .bind<ILanguageRepository>(TYPES.ILanguageRepository)
+    .to(LanguageRepository);
+container
+    .bind<IGameVersionGroupRepository>(TYPES.IGameVersionGroupRepository)
+    .to(GameVersionGroupRepository);
+container.bind<IRegionRepository>(TYPES.IRegionRepository).to(RegionRepository);
 container
     .bind<ISearchPokemonUsecase>(TYPES.ISearchPokemonUsecase)
     .to(SearchPokemonInteractor)
--- src/registory/inversify.types.ts
+++ src/registory/inversify.types.ts
@@ -1,6 +1,9 @@
 const TYPES = {
     PokemonController: Symbol.for('PokemonController'),
     IPokemonRepository: Symbol.for('IPokemonRepository'),
+    ILanguageRepository: Symbol.for('ILanguageRepository'),
+    IGameVersionGroupRepository: Symbol.for('IGameVersionGroupRepository'),
+    IRegionRepository: Symbol.for('IRegionRepository'),
     ISearchPokemonUsecase: Symbol.for('ISearchPokemonUsecase'),
     IPokemonPresenter: Symbol.for('IPokemonPresenter')
 } as const;
