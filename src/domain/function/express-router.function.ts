4d5d7de3c4936d0e7598b8c60fe0df4844f234e7
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-07-30 19:57:24 +0900

    router の設定処理を修正

---
+++ src/domain/function/express-router.function.ts
@@ -0,0 +1,39 @@
+import { Application, Router } from 'express';
+import {
+    isChildRouter,
+    isOperationRouter,
+    isSupportedHttpMethod
+} from '@/domain/function/router.function';
+
+const setRouter = (
+    app: Application,
+    routes: (OperationRouter | ChildRouter)[],
+    path: string = ''
+) => {
+    const router = Router();
+
+    routes.forEach(route => {
+        if (isChildRouter(route)) {
+            setRouter(app, route.children, `${path}${route.path}`);
+            return;
+        }
+
+        if (isOperationRouter(route) && isSupportedHttpMethod(route.method)) {
+            router.use((req, _, next) => {
+                console.log('[%o]: %s', new Date(), req.baseUrl);
+                next();
+            });
+
+            router
+                .route(route.path)
+                [route.method]((req, res) => route.action(req, res));
+        }
+    });
+
+    app.use(path, router);
+};
+
+export default (app: Application, appRoutes: AppRouter) => {
+    const { base, routes } = appRoutes;
+    setRouter(app, routes, base);
+};
