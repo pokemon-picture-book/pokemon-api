b61f4c3f63a7f0728722bef2d215ec80408b7cf3
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-01-18 21:32:18 +0900

    不要な相対パスを削除

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
         extensions: ['.ts', '.js', '.json'],
         alias: {
             '@': path.resolve(__dirname, 'src'),
-            '@t': path.resolve(__dirname, 'src', '@types'),
             '~': path.resolve(__dirname),
             '@test': path.resolve(__dirname, 'test')
         }
