e018a305aeb72e5e8fe4b32f72c8b5b9ca59019d
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-01-28 22:28:58 +0900

    非推奨の実装を修正

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import settingRouter from '@/domain/function/express-router.function';
 import appRoutes from '@/routes';
-import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import * as express from 'express';
 import { Application } from 'express';
@@ -9,13 +8,14 @@ const app: Application = express();
 
 const { NODE_ENV, REQUEST_LIMIT, SESSION_SECRET } = process.env;
 
-app.use(bodyParser.json({ limit: REQUEST_LIMIT || '100kb' }));
+app.use(express.json({ limit: REQUEST_LIMIT || '100kb' }));
 app.use(
-    bodyParser.urlencoded({
+    express.urlencoded({
         extended: true,
         limit: REQUEST_LIMIT || '100kb',
     })
 );
+
 app.use(cookieParser(SESSION_SECRET || 'mySecret'));
 
 if (NODE_ENV === 'development') {
