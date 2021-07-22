f0892b11f2422ac1afe29c50011b0d895109b40d
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-07-22 12:54:32 +0900

    サポート外のデータを更新するマイグレーションを追加

---
+++ db/migrations/1626925988640-update_table_game_version_group.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class updateTableGameVersionGroup1626925988640 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
